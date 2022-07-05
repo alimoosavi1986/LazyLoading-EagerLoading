8e16bae86b4619382110db1e1f4ee0a27091d44b
AM <a@b.com>
2022-07-05 22:53:56 +0330

    Add project files.

---
+++ LazyLoading-EagerLoading/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace LazyLoading_EagerLoading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLazyLoading = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEagerLoading = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLazyLoading
+            // 
+            this.btnLazyLoading.Location = new System.Drawing.Point(13, 64);
+            this.btnLazyLoading.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLazyLoading.Name = "btnLazyLoading";
+            this.btnLazyLoading.Size = new System.Drawing.Size(113, 28);
+            this.btnLazyLoading.TabIndex = 5;
+            this.btnLazyLoading.Text = "Lazy Loading";
+            this.btnLazyLoading.UseVisualStyleBackColor = true;
+            this.btnLazyLoading.Click += new System.EventHandler(this.btnLazyLoading_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 100);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(441, 164);
+            this.listBox1.TabIndex = 4;
+            // 
+            // btnEagerLoading
+            // 
+            this.btnEagerLoading.Location = new System.Drawing.Point(13, 13);
+            this.btnEagerLoading.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEagerLoading.Name = "btnEagerLoading";
+            this.btnEagerLoading.Size = new System.Drawing.Size(113, 28);
+            this.btnEagerLoading.TabIndex = 3;
+            this.btnEagerLoading.Text = "Eager Loading";
+            this.btnEagerLoading.UseVisualStyleBackColor = true;
+            this.btnEagerLoading.Click += new System.EventHandler(this.btnEagerLoading_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 278);
+            this.Controls.Add(this.btnLazyLoading);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEagerLoading);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLazyLoading;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEagerLoading;
+    }
+}
+
---
+++ LazyLoading-EagerLoading/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LazyLoading_EagerLoading
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+            students.Add(new Student { FirstName = "A", LastName = "A", Grade = 20 });
+            students.Add(new Student { FirstName = "B", LastName = "B", Grade = 18 });
+            students.Add(new Student { FirstName = "C", LastName = "C", Grade = 22 });
+            students.Add(new Student { FirstName = "D", LastName = "D", Grade = 19 });
+            students.Add(new Student { FirstName = "E", LastName = "E", Grade = 16 });
+            students.Add(new Student { FirstName = "F", LastName = "F", Grade = 8 });
+            students.Add(new Student { FirstName = "G", LastName = "G", Grade = 6 });
+        }
+
+        private List<Student> GetAcceptedStudentsEagerLoading()
+        {
+            List<Student> ResultSet = new List<Student>();
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Grade > 10)
+                {
+                    ResultSet.Add(students[i]);
+                }
+            }
+            return ResultSet;
+        }
+
+        private void btnEagerLoading_Click(object sender, EventArgs e)
+        {
+            var lst = GetAcceptedStudentsEagerLoading();
+            foreach (var st in lst)
+            {
+                listBox1.Items.Add(st.FirstName + "   " + st.LastName);
+            }
+        }
+
+        private IEnumerable<Student> GetAcceptedStudents()
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Grade > 10)
+                {
+                    yield return students[i];
+                }
+            }
+        }
+
+        private void btnLazyLoading_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Student> lst = GetAcceptedStudents();
+            foreach (var st in lst)
+            {
+                listBox1.Items.Add(st.FirstName + "   " + st.LastName);
+            }
+        }
+    }
+}
